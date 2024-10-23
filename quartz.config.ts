df0629418ffd9172ec38a5fb4987eb427e5e36aa
Comamoca <comamoca.dev@gmail.com>
2024-10-23 13:05:20 +0900

    :sparkles: (quartz.config.ts) Change language and fonts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,16 +15,16 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "ja-JP",
+    baseUrl: "note.comamoca.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Zen Kurenaido",
+        body: "Zen Kurenaido",
         code: "IBM Plex Mono",
       },
       colors: {
