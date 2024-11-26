5d8e44044b2e445250b65982cd82c1f2e9f92425
Comamoca <comamoca.dev@gmail.com>
2024-11-26 11:05:29 +0900

    :nail_care: (quartz.config.ts) Change font to Zen Maru Gothic

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Zen Kurenaido",
-        body: "Zen Kurenaido",
+        header: "Zen Maru Gothic",
+        body: "Zen Maru Gothic",
         code: "IBM Plex Mono",
       },
       colors: {
